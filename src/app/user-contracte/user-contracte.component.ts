b64ae26399a891611a99f4d04830b990b280a729
PatriciaGhitun <patriciaghitun@yahoo.com>
2020-01-04 00:24:30 +0200

    user-contracte component created
    +   new user added

---
+++ src/app/user-contracte/user-contracte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-contracte',
+  templateUrl: './user-contracte.component.html',
+  styleUrls: ['./user-contracte.component.css']
+})
+export class UserContracteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
